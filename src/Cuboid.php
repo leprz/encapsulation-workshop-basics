f2a93769118107635b1fcb54baa12f2df94c4ebf
Przemek <przemyslaw.leczycki@valueadd.pl>
2021-04-18 08:16:20 +0200

    Refactor 2

--- src/Cuboid.php
+++ src/Cuboid.php
@@ -6,6 +6,16 @@ class Cuboid
 {
     public function __construct(private float $a, private float $b, private float $h)
     {
+        self::assertGreaterThanZero($a);
+        self::assertGreaterThanZero($b);
+        self::assertGreaterThanZero($h);
+    }
+
+    private static function assertGreaterThanZero(float $num): void
+    {
+        if ($num < 0) {
+            throw new \InvalidArgumentException('Parameter must be greater than 0.');
+        }
     }
 
     public function volume(): float
--- src/Cylinder.php
+++ src/Cylinder.php
@@ -6,6 +6,15 @@ class Cylinder
 {
     public function __construct(private float $r, private float $h)
     {
+        self::assertGreaterThanZero($r);
+        self::assertGreaterThanZero($h);
+    }
+
+    private static function assertGreaterThanZero(float $num): void
+    {
+        if ($num < 0) {
+            throw new \InvalidArgumentException('Parameter must be greater than 0.');
+        }
     }
 
     public function volume(): float
